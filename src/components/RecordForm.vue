750b1212532006e01c1f276b1f30aadec3ab3891
sanggusseu <sanggusseu@gmail.com>
2024-10-02 16:32:10 +0900

    숫자, 지우기, .만 입력 가능하도록 수정

--- src/components/RecordForm.vue
+++ src/components/RecordForm.vue
@@ -5,14 +5,21 @@
       min="1"
       v-model="store.record"
       label="숨 참기 최고 기록 (초)"
+      :onkeydown="handleOnkeyDown"
     />
   </form>
 </template>
 
 <script setup>
 import { useRecordStore } from 'src/stores/record';
-
 const store = useRecordStore();
+
+const handleOnkeyDown = e => {
+  const num = e.key * 1;
+  const exclude = ['Backspace', 'Delete', '.'];
+  if (exclude.includes(e.key)) return;
+  if (isNaN(num)) e.preventDefault();
+};
 </script>
 
 <style lang="scss" scoped></style>
